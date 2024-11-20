4faf1f040c607c215e80f6b2789e7993691bceb5
Rowan Gibbard <rowangibbard01@gmail.com>
2024-11-20 14:22:35 +0000

    add js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const question = document. querySelector('#question')
-const choices = document. querySelectorAll('.choice-text')
-const question = document. querySelector('#question')
-const question = document. querySelector('#question')
-const question = document. querySelector('#question')
+const question = document. querySelector('#question') //returns the first Element within the document that matches the specified CSS selector, or group of CSS selectors
+const choices = Array.from(document.querySelectorAll('.choice-text')) //querySelectorAll() returns a static (not live) NodeList representing a list of the document's elements that match the specified group of selectors, then array from creates an array from them.
+const progressText = document. querySelector('#progressText')
+const scoreText = document. querySelector('#score')
+const progressBarFull = document. querySelector('#progressBarFull')
