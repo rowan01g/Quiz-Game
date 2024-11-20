bf30f233dfe69996b150070d66f23c4e5b38544f
Rowan Gibbard <rowangibbard01@gmail.com>
2024-11-20 12:13:21 +0000

    add js

---
+++ game.js
@@ -0,0 +1,5 @@
+const question = document. querySelector('#question')
+const choices = document. querySelectorAll('.choice-text')
+const question = document. querySelector('#question')
+const question = document. querySelector('#question')
+const question = document. querySelector('#question')
