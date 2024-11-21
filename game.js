9f908e49215103266e3be6c08c3dae87036bc5e9
Rowan Gibbard <rowangibbard01@gmail.com>
2024-11-21 13:42:07 +0000

    add js

--- game.js
+++ game.js
@@ -93,5 +93,22 @@ getNewQuestion = () => {
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if(!acceptingAnswers) return // "if not accepting answers" When a return statement is encountered within a function, the function will immediately stop executing
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct': 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
     })
 })
